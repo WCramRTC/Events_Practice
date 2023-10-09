52b2b7dd47d8d6dab0551556a8ef5ce7aaf0b6d6
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-09 16:17:22 -0700

    Add project files.

---
+++ MessageService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Practice
+{
+    internal class MessageService
+    {
+        // Step 1: Declare the delegate
+        public delegate void MessageReceivedHandler(string message);
+
+        // Step 2: Declare the event using the delegate
+        public event MessageReceivedHandler MessageReceived;
+
+        // Method to simulate receiving a message and raising the event
+
+
+        public void ReceiveMessage(string message)
+        {
+            Console.WriteLine("MessageService: Received a message.");
+
+            // Step 3: Raise the event
+            MessageReceived?.Invoke(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Events_Practice
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var messageService = new MessageService();
+
+            // Step 4: Subscribe to the event
+            messageService.MessageReceived += OnMessageReceived;
+
+            // Simulate receiving a message
+            messageService.ReceiveMessage("Hello, World!");
+
+            Console.ReadKey();
+        }
+
+        // Event handler
+        static void OnMessageReceived(string message)
+        {
+            Console.WriteLine($"Program: Message received - {message}");
+        }
+
+    }
+}
